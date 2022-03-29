6491df27b51897c2a9ff6f3518c38f501842a377
a995049470 <17855834269@163.com>
2022-03-29 18:24:41 +0800

    1.删掉老的 重写一套简易虚拟机

--- Assets/Code/NullFramework/Runtime/Game/Tree/BehaviorLeaf.cs
+++ Assets/Code/NullFramework/Runtime/Game/Tree/BehaviorLeaf.cs
@@ -5,21 +5,7 @@ namespace NullFramework.Runtime
     public class BehaviorLeaf : Leaf
     {
         private ByteCodeBehavior m_behavior;
-        public bool TryGetResult(out bool isSucess)
-        {
-            isSucess = false;
-            bool isComplte = false;
-            if(m_behavior == null)
-            {
-                isComplte = true;
-            }
-            else if(m_behavior.IsEnd())
-            {
-                isSucess = m_behavior.GetExcuteResult();
-                isComplte = true;
-            }
-            return isComplte;
-        }
+        
         public void AddBehaviorUpdate(ByteCodeBehavior behavior, Action action)
         {
             m_behavior = behavior;
